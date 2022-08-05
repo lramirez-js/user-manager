625fc338b2df4b63e0a320561cdc0f4df2c92c11
lramirez-js <luis.rg@outlook.com>
2022-08-05 06:48:30 -0500

    Card component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import useForm from './hooks/useForm';
 import Input from './components/Input'
+import Card from './components/Card'
 
 function App() {
   const [ form, handleChange] = useForm({ name: '', lastname: '' })
   console.log(form)
 
   return (
-    <form>
-      <Input 
-        name="name" 
-        label="Name"
-        value={form.name} 
-        onChange={handleChange} />
-      <Input 
-        name="lastname" 
-        label="Lastame"
-        value={form.lastname} 
-        onChange={handleChange} />
-    </form>
+    <Card>
+      <form>
+        <Input 
+          name="name" 
+          label="Name"
+          value={form.name} 
+          onChange={handleChange} />
+        <Input 
+          name="lastname" 
+          label="Lastame"
+          value={form.lastname} 
+          onChange={handleChange} />
+      </form>
+    </Card>
   );
 }
 
