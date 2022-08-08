283da3bec48e430e477721ea3da395276de70afe
lramirez-js <luis.rg@outlook.com>
2022-08-08 06:56:11 -0500

    Container component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import useForm from './hooks/useForm';
 import Input from './components/Input'
 import Card from './components/Card'
+import Container from './components/Container'
 
 function App() {
   const [ form, handleChange] = useForm({ name: '', lastname: '' })
   console.log(form)
 
   return (
-    <Card>
-      <form>
-        <Input 
-          name="name" 
-          label="Name"
-          value={form.name} 
-          onChange={handleChange} />
-        <Input 
-          name="lastname" 
-          label="Lastame"
-          value={form.lastname} 
-          onChange={handleChange} />
-      </form>
-    </Card>
+    <Container>
+      <Card>
+        <div style={{ padding: '20px' }}>
+          <form>
+            <Input 
+              name="name" 
+              label="Name"
+              value={form.name} 
+              onChange={handleChange} />
+            <Input 
+              name="lastname" 
+              label="Lastame"
+              value={form.lastname} 
+              onChange={handleChange} />
+          </form>
+        </div>
+      </Card>
+    </Container>
   );
 }
 
